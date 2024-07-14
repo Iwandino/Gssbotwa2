bb4a7185b0a0be38ee419913e536f1c581ee163d
Iwandino <150587438+Iwandino@users.noreply.github.com>
2024-07-14 13:18:14 +0800

    Update config.js

--- config.js
+++ config.js
@@ -4,7 +4,7 @@ const chalk = require('chalk');
 
 // Other
 global.link = 'https://whatsapp.com/channel/0029VaWJMi3GehEE9e1YsI1S'
-global.linkGroup = 'https://chat.whatsapp.com/E3PWxdvLc7ZCp1ExOCkEGp'
+global.linkGroup = ''
 
 global.apikey = 'bf2d2cf29b3edc604b447983';
 global.ownername = "iwandino";
@@ -34,15 +34,15 @@ global.mess = {
     success: '`✓ Success`',
     admin: '`This feature is only for group admins`',
     botAdmin: '`I am not an admin!`',
-    owner: '`You are not my owner`',
+    owner: '`You are not my owner you gay`',
     group: '`You can use this command only in groups ❌`',
     private: '`Feature is used only for private chats!`',
     bot: '`Bot number user special features`',
-    wait: '*Processing Your request*',
+    wait: '*Processing Your request bitch*',
     endLimit: '`Your daily limit has expired, the limit will be reset every 12 hours`',
 };
 
 global.limitawal = {
     premium: 'Infinity',
-    free: 10
+    free: 100
 };
