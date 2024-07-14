5894eb298f15dcbca6e8526bc2be1be5d1e37992
Iwandino <150587438+Iwandino@users.noreply.github.com>
2024-07-14 18:40:50 +0800

    Update config.js

--- config.js
+++ config.js
@@ -3,8 +3,8 @@ const chalk = require('chalk');
 
 
 // Other
-global.link = 'https://https://whatsapp.com/channel/0029VajUvHu8PgsPaLHsml11'
-global.linkGroup = 'https://chat.whatsapp.com/GRWkaQmzVVjLdTjagqcf8F'
+global.link = 'https://whatsapp.com/channel/0029VaWJMi3GehEE9e1YsI1S'
+global.linkGroup = 'https://chat.whatsapp.com/E3PWxdvLc7ZCp1ExOCkEGp'
 
 global.apikey = 'bf2d2cf29b3edc604b447983';
 global.ownername = "iwandino";
